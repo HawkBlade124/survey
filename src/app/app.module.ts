a05c1c83107f09614c8898d22d2c26f2aedb14a8
Ben Fuller <Benfuller@fullerview.net>
2019-03-26 17:11:36 -0600

    Enhancing the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResponsesComponent } from './responses/responses.component';
+import { SurveysComponent } from './surveys/surveys.component';
+import { HeadersComponent } from './headers/headers.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+const appRoutes: Routes =[
+  {path: '', component: SurveysComponent},
+  {path: 'surveys', component: SurveysComponent},
+  {path: 'responses', component: ResponsesComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResponsesComponent,
+    SurveysComponent,
+    HeadersComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/responses/response.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResponseListComponent } from './response-list/response-list.component';
+
+@NgModule({
+  declarations: [ResponseListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ResponseModule { }
